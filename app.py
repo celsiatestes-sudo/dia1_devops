6848beac495205b37fdeda28fbfc605ac6b9284e
celsiadev <celsiatestes@gmail.com>
2025-10-09 17:02:15 +0100

    Refatorando o arquivo o arquivo app.py

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-nome = input("Digite seu nome: ")
-idade1 = int(input("Digite a primeira idade: "))
-idade2 = int(input("Digite a segunda idade: "))
+#Definindo as funcao do programa ,funçoes testáveis 
+def soma(a,b):
+     return a+b
 
-soma=idade1+idade2
+def mult(a,b):
+    return a*b
 
-if soma<15:
-    print("Voce nao pode entrar",soma)
-elif soma>=15 and soma<18:
-    print("Voce pode entrar mas com um adulto",soma )
-else:
-    print("Voce pode entrar já es maior de idade")
+def div(a,b):
+    #tratando excesso
+    if a==0 and b==0:
+        raise ZeroDivisionError("O numero nao pode ser zero")
+    return a/b
 
- #Adicionando funcao multiplicacao
- 
-def mult():
-    return idade1*idade2
-print("A multi desta idade é", mult())
+if __name__=="__main__":
 
-#Adicionando divisão 
-def div():
-    return idade1/idade2
-print("O resultado da divisão entre as idades é",div())
+    nome = input("Digite seu nome: ")
+    idade1 = int(input("Digite a primeira idade: "))
+    idade2 = int(input("Digite a segunda idade: "))
 
+    #nao é possivel comparar a funcao por isso demos variavel ao resultado de soma
 
+    soma_idade=soma(idade1,idade2)
+    
+
+    if soma_idade<15:
+        print("Voce nao pode entrar",soma_idade)
+    elif soma_idade>=15 and soma_idade<18:
+        print("Voce pode entrar mas com um adulto",soma_idade )
+    else:
+        print("Voce pode entrar já es maior de idade")
+
+    print("O resultado da soma entre as idades é",soma_idade)
+    print("O resultado da divisao entre as idades é",div(idade1,idade2))
+    print("A multiplicacao desta idade é", mult(idade1,idade2))
